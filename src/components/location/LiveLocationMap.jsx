29ac4fc61909c334a3629e2be0348d06b7fceb46
gilad frimet-Computers Service <frimet@gmail.com>
2025-12-23 15:03:57 +0000

    File changes

--- src/components/location/LiveLocationMap.jsx
+++ src/components/location/LiveLocationMap.jsx
@@ -233,8 +233,9 @@ export default function LiveLocationMap({ trip, currentUserEmail, onUpdate }) {
                 style={{ height: '100%', width: '100%' }}
               >
                 <TileLayer
-                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  url="https://israelhiking.osm.org.il/Hebrew/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="https://israelhiking.osm.org.il">Israel Hiking</a> | <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                  maxZoom={16}
                 />
 
                 {/* Trip starting point */}
--- src/components/maps/MapSidebar.jsx
+++ src/components/maps/MapSidebar.jsx
@@ -131,8 +131,9 @@ export default function MapSidebar({ trip, isOrganizer, onUpdate }) {
                     style={{ height: '100%', width: '100%' }}
                   >
                     <TileLayer
-                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      url="https://israelhiking.osm.org.il/Hebrew/{z}/{x}/{y}.png"
+                      attribution='&copy; <a href="https://israelhiking.osm.org.il">Israel Hiking</a> | <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                      maxZoom={16}
                     />
                     
                     {/* Starting point marker */}
